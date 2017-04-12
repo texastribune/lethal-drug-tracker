6778b133f0ec1c337ed553bbb7c2be783d80c14b
jolie_sky <jolie.mccullough@gmail.com>
2017-04-12 17:57:28 -0500

    play with leading numbers

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,8 +12,10 @@ window.addEventListener("resize", debounce(buildChart, 300));
 //makes dots the same height as width
 function makeDots() {
     var width = document.getElementsByClassName('dose-dot')[0].clientWidth;
+    var margin = 16;
+
     var elements = document.getElementsByClassName('dose-dot');
     for (var i = 0; i < elements.length; i++) {
-        elements[i].style.height=(width+"px");
+        elements[i].style.height=(width+margin+'px');
     }
 }

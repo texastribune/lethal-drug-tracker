be6e5f45ac36189b06b37beff86817b298648a2f
jolie_sky <jolie.mccullough@gmail.com>
2017-04-13 18:08:01 -0500

    messing with the logs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,8 +6,10 @@ import debounce from './utils/debounce'
 
 onDocumentReady(makeDots);
 onDocumentReady(buildChart);
-window.addEventListener("resize", debounce(makeDots, 200));
-window.addEventListener("resize", debounce(buildChart, 300));
+// onDocumentReady(showLog);
+document.getElementById('more-log').addEventListener('click', showLog);
+window.addEventListener('resize', debounce(makeDots, 200));
+window.addEventListener('resize', debounce(buildChart, 300));
 
 //makes dots the same height as width
 function makeDots() {
@@ -18,4 +20,19 @@ function makeDots() {
     for (var i = 0; i < elements.length; i++) {
         elements[i].style.height=(width+margin+'px');
     }
+}
+
+//show more past log
+function showLog() {
+  var logs = document.getElementsByClassName('log--item')
+  var logLength = logs.length
+  var moreButton = document.getElementById('more-log')
+  //get first hidden ID and start from there?
+
+  //how to get to show only some more?
+  for(var i=1; i <= logLength; i++) {
+    document.getElementById('loop-'+i).classList.remove('hidden')
+  }
+
+  moreButton.classList.add('hidden')
 }

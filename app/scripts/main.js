f089d9f98e629c6455110d334f128c39eb376d3e
jolie_sky <jolie.mccullough@gmail.com>
2017-04-13 16:06:20 -0500

    new display

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,7 +12,7 @@ window.addEventListener("resize", debounce(buildChart, 300));
 //makes dots the same height as width
 function makeDots() {
     var width = document.getElementsByClassName('dose-dot')[0].clientWidth;
-    var margin = 16;
+    var margin = 10;
 
     var elements = document.getElementsByClassName('dose-dot');
     for (var i = 0; i < elements.length; i++) {
